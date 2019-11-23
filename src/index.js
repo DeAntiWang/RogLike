4f05929bd3628b803544cf61effa93451ff69cea
DarkKris <long4664030@163.com>
2019-11-23 19:27:21 +0800

    feat: draw the first scene

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-window.onload = () => {
-    initScene();
-    Nipple();
-};
-
 let Engine = Matter.Engine,
     World = Matter.World,
-    Runner = Matter.Runner,
+    Vector = Matter.Vector,
     Render = Matter.Render,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Body = Matter.Body;
+let appWidth,
+    appHeight,
+    wholeHeight;
+let me = null;
 
 function initScene() {
-    let appWidth = document.body.clientWidth;
-    let appHeight = document.body.clientHeight-140;
-
-    let engine = Engine.create();
+    let engine = Engine.create({
+        enableSleeping: true    // 开启睡眠模式，提高引擎效率
+    });
     let options = {
         width: appWidth,
         height: appHeight,
+        wireframes: false,
+        background: '#ffffff',
         hasBounds: true,
         showVelocity: true
     };
@@ -24,14 +25,26 @@ function initScene() {
     // 创建刚体
     let rect = Bodies.rectangle(200, 100, 50, 50), // 矩形
         circle = Bodies.circle(300, 100, 25), // 圆
-        polygon = Bodies.polygon(450, 100, 5, 25), // 多边形
-        trapezoid = Bodies.trapezoid(590, 100, 50, 50, 3), // 梯形
-        ground = Bodies.rectangle(400, 580, 790, 60, {isStatic: true}),
-        me = Bodies.circle(50, document.body.clientHeight-400, 20);
+        leftWall = Bodies.rectangle(-2, appHeight/2, 1, appHeight, {
+            isStatic: true
+        }), // 左墙
+        rightWall = Bodies.rectangle(appWidth+2, appHeight/2, 1, appHeight, {
+            isStatic: true
+        }), // 右墙
+        ceil = Bodies.rectangle(appWidth/2, -2, appWidth, 1, {
+            isStatic: true
+        }), // 天花板
+        ground = Bodies.rectangle(appWidth/2, 580, appWidth, 32, {
+            isStatic: true,
+            background: 'black'
+        }); // 地板
+    me = Bodies.circle(50, wholeHeight-400, 20, {
+        density: 0.68, // 密度
+        restitution: 0.4 // 弹性
+    });
 
     // 将刚体添加到世界中
-    World.add(engine.world, [rect, circle, me, ground]);
-
+    World.add(engine.world, [rect, circle, me, ground, leftWall, rightWall, ceil]);
 
     let render = Render.create({
         element: document.body,
@@ -43,11 +56,34 @@ function initScene() {
     Render.run(render);
 }
 
-function Nipple() {
+function initNipple() {
     let controller = nipplejs.create({
         zone: document.getElementById('static'),
         mode: 'static',
-        position: {left: '50%', top: 'calc(100% - 70px)'},
-        color: 'rgb(40,40,40)'
+        position: {left: 'calc(100% - 80px)', top: 'calc(100% - 70px)'},
+        color: 'rgb(180,180,180)'
+    });
+    controller.on('move', function(evt, data) {
+        let x = data.position.x-appWidth+80;
+        // 给横向加速度
+        let force = Vector.create(x*0.005, 0);
+        addForce(force)
     });
 }
+
+function addForce(force) {
+    Body.applyForce(me, me.position, force);
+}
+
+function bounce() {
+    console.log(me);
+    Body.applyForce(me, me.position, {x: 0, y: -50*0.51});
+}
+
+window.onload = () => {
+    appWidth = document.body.clientWidth;
+    appHeight = document.body.clientHeight-140;
+    wholeHeight = document.body.clientHeight;
+    initScene();
+    initNipple();
+};
