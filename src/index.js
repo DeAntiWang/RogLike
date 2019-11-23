90f5e34f58e8462cd3480c28fd6f5ca36adde149
DarkKris <long4664030@163.com>
2019-11-23 23:00:20 +0800

    feat: 进一步更新

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ let Engine = Matter.Engine,
     Vector = Matter.Vector,
     Render = Matter.Render,
     Bodies = Matter.Bodies,
-    Body = Matter.Body;
+    Body = Matter.Body,
+    Events = Matter.Events,
+    Composite = Matter.Composite;
+                        // Matter.Js
 let appWidth,
     appHeight,
-    wholeHeight;
-let me = null;
+    wholeHeight;        // app属性
+let me=null,
+    wall=null,
+    dwall=null;         // 地图元素
+let canBounce = false;  // 是否可以使用跳跃
+let firstStart = true;  // 是否还未开始移动
+let historyPath = [];   // 历史路径
 
+// 物理引擎
 function initScene() {
+    // 创建引擎
     let engine = Engine.create({
         enableSleeping: true    // 开启睡眠模式，提高引擎效率
     });
+    // Render配置项
     let options = {
         width: appWidth,
         height: appHeight,
@@ -22,10 +33,54 @@ function initScene() {
         showVelocity: true
     };
 
+    initMap();
+
+    // 安全墙体内部成员ID
+    let wallsId = Composite.allBodies(wall).map(x => x.id);
+    // 危险墙体内部成员ID
+    let dwallsId = Composite.allBodies(dwall).map(x => x.id);
+
+
+    // 把元素添加到世界中
+    World.add(engine.world, [me, wall, dwall]);
+
+    // 创建Render
+    let render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: options
+    });
+
+    // 运行引擎和Render
+    Engine.run(engine);
+    Render.run(render);
+
+    // 监听碰撞事件
+    Events.on(engine, "collisionStart", (evt) => {
+        evt.source.broadphase.pairsList.forEach( val => {
+            // 判断me和安全墙体相撞
+            if(
+                (val[0].id===me.id && wallsId.indexOf(val[1].id)!==-1) ||
+                (val[1].id===me.id && wallsId.indexOf(val[0].id)!==-1)
+            ) {
+                canBounce = true;
+            }
+            // 判断me和危险墙体相撞
+            if(
+                (val[0].id===me.id && dwallsId.indexOf(val[1].id)!==-1) ||
+                (val[1].id===me.id && dwallsId.indexOf(val[0].id)!==-1)
+            ) {
+                // TODO 死亡逻辑
+                console.log('death');
+            }
+        });
+    });
+}
+
+// 初始化地图
+function initMap() {
     // 创建刚体
-    let rect = Bodies.rectangle(200, 100, 50, 50), // 矩形
-        circle = Bodies.circle(300, 100, 25), // 圆
-        leftWall = Bodies.rectangle(-2, appHeight/2, 1, appHeight, {
+    let leftWall = Bodies.rectangle(-2, appHeight/2, 1, appHeight, {
             isStatic: true
         }), // 左墙
         rightWall = Bodies.rectangle(appWidth+2, appHeight/2, 1, appHeight, {
@@ -36,26 +91,32 @@ function initScene() {
         }), // 天花板
         ground = Bodies.rectangle(appWidth/2, 580, appWidth, 32, {
             isStatic: true,
-            background: 'black'
+            background: '#000000'
         }); // 地板
+    let test = Bodies.rectangle(appWidth/2, 530, 50, 50, {
+        isStatic: true
+    });
+
+    // 主角
     me = Bodies.circle(50, wholeHeight-400, 20, {
-        density: 0.68, // 密度
-        restitution: 0.4 // 弹性
+        density: 1, // 密度
+        restitution: 0 // 弹性
     });
 
-    // 将刚体添加到世界中
-    World.add(engine.world, [rect, circle, me, ground, leftWall, rightWall, ceil]);
+    // 安全墙体
+    wall = Composite.create();
+    Composite.add(wall, leftWall);
+    Composite.add(wall, rightWall);
+    Composite.add(wall, ground);
+    Composite.add(wall, ceil);
 
-    let render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: options
-    });
-
-    Engine.run(engine);
-    Render.run(render);
+    // 危险墙体
+    dwall = Composite.create();
+    Composite.add(dwall, test);
 }
 
+
+// 摇杆
 function initNipple() {
     let controller = nipplejs.create({
         zone: document.getElementById('static'),
@@ -69,15 +130,38 @@ function initNipple() {
         let force = Vector.create(x*0.005, 0);
         addForce(force)
     });
+    controller.on('start', function() {
+        if(firstStart) {
+            // TODO 开始记录路径
+            return historyPath.push([]);
+            let id = savePath(array, me);
+            setTimeout(()=>{clearInterval(id); console.log('oh',array)},1000);
+            firstStart=false;
+        }
+    });
 }
 
+// 给主角施加力
 function addForce(force) {
     Body.applyForce(me, me.position, force);
 }
-
+// 主角弹跳纵向力
 function bounce() {
-    console.log(me);
-    Body.applyForce(me, me.position, {x: 0, y: -50*0.51});
+    if(canBounce) {
+        Body.applyForce(me, me.position, {x: 0, y: -50*0.51});
+        canBounce=false;
+    }
+}
+
+// 记录第n位角色路径
+function savePath(array, obj) {
+    let time = 0;
+    return setInterval( () => {
+        array.push({
+            ...obj.position,
+            time: time+=10
+        });
+    }, 20);
 }
 
 window.onload = () => {
